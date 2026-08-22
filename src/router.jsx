c71f9087733052668707a16087ef5130f89ee2ae
plotagons84-hub <plotagons84@gmail.com>
2026-08-22 20:47:10 +0530

    Delete src directory

--- src/router.jsx
+++
@@ -1,7 +0,0 @@
-import { createRouter } from '@tanstack/react-router'
-import { rootRoute } from './routes/__root'
-import { indexRoute } from './routes/index'
-
-const routeTree = rootRoute.addChildren([indexRoute])
-
-export const router = createRouter({ routeTree })
